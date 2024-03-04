fbc9583d3269f7c1c5481a679e6610556fb3e889
Mateusz Kubiak <matkubiak888@gmail.com>
2024-03-04 21:34:45 +0000

    elefele

---
+++ src/pages/MainPage.js
@@ -0,0 +1,53 @@
+import React from 'react';
+
+const MainPage = () => {
+  return (
+    <div>
+      <h1>Project Presentation</h1>
+      <h2>Context:</h2>
+      
+      <h2>Problem:</h2>
+      
+      <h2>Goals:</h2>
+      
+      <h2>Related Work:</h2>
+      
+      <h2>Expected Results:</h2>
+
+      <h1>Project Calendar</h1>
+      <h2>Tasks List:</h2>
+      
+      <h2>Project Schedule:</h2>
+      
+      <h2>Milestones:</h2>
+      
+      <h2>Deliverables:</h2>
+
+      <h1>Communication Plan</h1>
+      <h2>Project Website and Repository:</h2>
+      
+      <h2>Project Plan:</h2>
+      
+      <h2>Reports and Presentations:</h2>
+      
+      <h2>Role and List of Activities by Team Member:</h2>
+      
+      <h2>Repository (Git):</h2>
+
+      <h1>Agile Software Development</h1>
+      <h2>Agile Manifesto Statements:</h2>
+      
+      <h2>Agile Teams:</h2>
+      
+      <h1>To Do List</h1>
+      <ul>
+        <li>Project Presentation</li>
+        <li>Project Calendar</li>
+        <li>Communication Plan</li>
+        <li>Project Website (code UA)</li>
+      </ul>
+    </div>
+  );
+}
+
+export default MainPage;
